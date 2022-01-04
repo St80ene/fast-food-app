568ed0160247f35d2fb5b4f746aff34259812b6b
etiene <etienejames5@gmail.com>
2022-01-04 19:49:57 +0100

    fix nav bar styling

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './NavBar.module.scss';
 import { MenuItems } from '../MenuItems';
 import { BiMenuAltRight } from 'react-icons/bi';
 import { AiOutLineClose } from 'react-icons/ai';
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
     <header className={classes.header}>
       <div className={classes.header__content}>
-        <h2>Food Corner</h2>
-      </div>
-      <nav className={classes.header__content__nav}>
-        <ul>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a href="#">{item}</a>
-              </li>
-            );
-          })}
-        </ul>
-        <button>CTA Page</button>
+        <h2 className={classes.header__content__logo}>Food Corner</h2>
+
+        <nav
+          className={`${classes.header__content__nav} ${
+            menuOpen ? classes.isMenu : ''
+          }`}
+        >
+          <ul>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a href="#">{item}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <button>CTA Page</button>
+        </nav>
         <div className={classes.header__content__toggle}>
           <BiMenuAltRight />
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
