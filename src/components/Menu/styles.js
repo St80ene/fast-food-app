99ada5b05440e675b30e488e03dbda9c4305eee8
etiene <etienejames5@gmail.com>
2022-02-01 00:39:41 +0100

    added card skeletons to menu page

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -7,13 +7,61 @@ const Container = styled.div`
   text-align: center;
 `;
 
-const ImgContainer = styled.img`
-  width: 18rem;
-  height: 18rem;
+const Heading = styled.h2`
+  margin-top: 1.2rem;
+  text-align: center;
+  color: #0000008f;
+
+  @media screen and (min-width: 850px) {
+    font-size: 17px;
+    margin-top: 2.2rem;
+  }
 `;
 
-const ImgWrapper = styled.div`
-  margin-top: 8rem;
+const CardContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: 850px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
 `;
 
-export { Container, ImgContainer, ImgWrapper };
+const Card = styled.div`
+  & > img {
+    height: auto;
+    width: 100%;
+  }
+  margin: 1rem auto;
+  border: 1px solid gray;
+  border-radius: 4px;
+  width: 96%;
+  min-height: 300px;
+  position: relative;
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  @media screen and (min-width: 850px) {
+    width: 23rem;
+  }
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  /* background: linear-gradient(89.91deg, rgba(0, 0, 0, 0) 0.03%, #033154 40.1%); */
+  /* opacity: 0.7; */
+  /* transform: matrix(-1, 0, 0.01, -1, 0, 0); */
+`;
+
+export { Container, Heading, CardContainer, Card, Overlay };
